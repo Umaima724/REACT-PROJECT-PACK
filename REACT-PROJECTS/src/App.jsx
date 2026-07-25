8131aec9e7b390e75ad98c56a2e89c6adc9a4df2
Umaima724 <umaimamubashar19@gmail.com>
2026-07-25 13:06:31 +0500

    created load data

--- REACT-PROJECTS/src/App.jsx
+++ REACT-PROJECTS/src/App.jsx
@@ -4,6 +4,7 @@ import Accordian from './components/accordian'
 import RandomColor from "./components/random-color";
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
 function App() {
   
 
@@ -19,6 +20,7 @@ function App() {
         page={1}
         limit={5}
       />
+      <LoadMoreData/>
       </div>
     </>
   )
